6f3a42f28d45a1588b1f37d3208766a0284b171e
Rustle Karl <fu.jiawei@outlook.com>
2020-09-24 21:10:15 +0800

    add: Generates User-Agent HTTP header

---
+++ pkgs/user_agent/constants.py
@@ -0,0 +1,308 @@
+import json
+import os.path
+
+DEVICE_TYPE_OS = {
+    'desktop': ('win', 'mac', 'linux'),
+    'smartphone': ('android', 'ios'),
+}
+
+OS_DEVICE_TYPE = {
+    'win': ('desktop',),
+    'linux': ('desktop',),
+    'mac': ('desktop',),
+    'android': ('smartphone',),
+    'ios': ('smartphone',),
+}
+
+DEVICE_TYPE_NAVIGATOR = {
+    'desktop': ('chrome', 'firefox', 'ie', 'edge', 'safari', 'opera'),
+    'smartphone': ('firefox', 'chrome', 'safari', 'opera'),
+}
+
+NAVIGATOR_DEVICE_TYPE = {
+    'ie': ('desktop',),
+    'edge': ('desktop',),
+    'chrome': ('desktop', 'smartphone'),
+    'firefox': ('desktop', 'smartphone'),
+    'safari': ('desktop', 'smartphone'),
+    'opera': ('desktop', 'smartphone'),
+}
+
+OS_NAVIGATOR = {
+    'win': ('chrome', 'firefox', 'ie', 'edge', 'opera'),
+    'mac': ('chrome', 'firefox', 'safari', 'opera'),
+    'linux': ('chrome', 'firefox', 'opera'),
+    'android': ('chrome', 'firefox', 'opera'),
+    'ios': ('chrome', 'firefox', 'safari', 'opera'),
+}
+
+NAVIGATOR_OS = {
+    'chrome': ('win', 'linux', 'mac', 'android', 'ios'),
+    'firefox': ('win', 'linux', 'mac', 'android', 'ios'),
+    'opera': ('win', 'linux', 'mac', 'android', 'ios'),
+    'safari': ('ios', 'mac'),
+    'edge': ('win',),
+    'ie': ('win',),
+}
+
+OS_PLATFORM = {
+    # https://en.wikipedia.org/wiki/Windows_NT#Releases
+    'win': (
+        'Windows NT 5.1',  # Windows XP
+        'Windows NT 6.1',  # Windows 7
+        'Windows NT 6.2',  # Windows 8
+        'Windows NT 6.3',  # Windows 8.1
+        'Windows NT 10.0',  # Windows 10
+    ),
+    # https://en.wikipedia.org/wiki/Macintosh_operating_systems#Releases_2
+    'mac': (
+        'Macintosh; Intel Mac OS X 10.8',
+        'Macintosh; Intel Mac OS X 10.9',
+        'Macintosh; Intel Mac OS X 10.10',
+        'Macintosh; Intel Mac OS X 10.11',
+        'Macintosh; Intel Mac OS X 10.12',
+        'Macintosh; Intel Mac OS X 10.13',  # 2017-9-25
+        'Macintosh; Intel Mac OS X 10.14',  # 2018-9-24
+    ),
+    'linux': (
+        'X11; Linux',
+        'X11; Ubuntu; Linux',
+        'X11; Debian; Linux',
+    ),
+    # https://en.wikipedia.org/wiki/Android_(operating_system)
+    'android': (
+        # 'Android 4.4', # 2013-10-31
+        # 'Android 4.4.1', # 2013-12-05
+        # 'Android 4.4.2', # 2013-12-09
+        # 'Android 4.4.3', # 2014-06-02
+        # 'Android 4.4.4', # 2014-06-19
+        # 'Android 5.0', # 2014-11-12
+        # 'Android 5.0.1', # 2014-12-02
+        # 'Android 5.0.2', # 2014-12-19
+        # 'Android 5.1', # 2015-03-09
+        'Android 5.1.1',  # 2015-04-21
+        'Android 6.0',  # 2015-10-05
+        'Android 6.0.1',  # 2015-12-07
+        'Android 7.0',  # 2016-08-22
+        'Android 7.1',  # 2016-10-04
+        'Android 7.1.1',  # 2016-12-05
+        'Android 8.0',  # 2017-8-21
+        'Android 8.1',  # 2017-12-5
+        'Android 9',  # 2018-8-6
+    ),
+    'ios': None,
+}
+
+# https://en.wikipedia.org/wiki/MacOS#Release_history
+MACOSX_CHROME_BUILD_RANGE = {
+    '10.8': (0, 5),
+    '10.9': (0, 5),
+    '10.10': (0, 5),
+    '10.11': (0, 6),
+    '10.12': (0, 6),
+    '10.13': (0, 6),
+    '10.14': (0, 2),
+}
+
+OS_CPU = {
+    'win': (
+        '',  # 32bit
+        'Win64; x64',  # 64bit
+        'WOW64',  # 32bit process on 64bit system
+    ),
+    'linux': (
+        'i686',  # 32bit
+        'x86_64',  # 64bit
+        'i686 on x86_64',  # 32bit process on 64bit system
+    ),
+    'android': (
+        'armv7l',  # 32bit
+        'armv8l',  # 64bit
+    ),
+}
+
+# https://en.wikipedia.org/wiki/History_of_Firefox
+FIREFOX_VERSION = (
+    # '45.0', # 2016-3-8
+    # '46.0', # 2016-4-26
+    # '47.0', # 2016-6-7
+    # '48.0', # 2016-8-2
+    # '49.0', # 2016-9-20
+    # '50.0', # 2016-11-15
+    # '51.0', # 2017-1-24
+    # '52.0', # 2017-3-7
+    # '53.0', # 2017-4-19
+    '54.0',  # 2017-6-13
+    '55.0',  # 2017-8-8
+    '56.0',  # 2017-9-28
+    '57.0',  # 2017-11-14
+    '58.0',  # 2018-1-23
+    '59.0',  # 2018-3-13
+    '60.0',  # 2018-5-9
+    '61.0',  # 2018-6-26
+    '62.0',  # 2018-9-5
+    '63.0',  # 2018-10-23
+    '64.0',  # 2018-12-11
+)
+
+# https://en.wikipedia.org/wiki/Google_Chrome_version_history
+CHROME_BUILD = (
+    # (49, 2623, 2660),  # 2016-03-02
+    # (50, 2661, 2703),  # 2016-04-13
+    # (51, 2704, 2742),  # 2016-05-25
+    # (52, 2743, 2784),  # 2016-07-20
+    # (53, 2785, 2839),  # 2016-08-31
+    # (54, 2840, 2882),  # 2016-10-12
+    # (55, 2883, 2923),  # 2016-12-01
+    # (56, 2924, 2986),  # 2016-12-01
+    # (57, 2987, 3028),  # 2017-03-09
+    # (58, 3029, 3070),  # 2017-04-19
+    (59, 3071, 3111),  # 2017-06-05
+    (60, 3112, 3162),  # 2017-07-25
+    (61, 3163, 3201),  # 2017-09-05
+    (62, 3202, 3238),  # 2017-10-17
+    (63, 3239, 3281),  # 2017-12-06
+    (64, 3282, 3324),  # 2018-01-24
+    (65, 3325, 3358),  # 2018-03-06
+    (66, 3359, 3395),  # 2018-04-17
+    (67, 3396, 3439),  # 2018-05-29
+    (68, 3440, 3496),  # 2018-07-24
+    (69, 3497, 3537),  # 2018-09-04
+    (70, 3538, 3577),  # 2018-10-16
+    (71, 3578, 3626),  # 2018-12-04
+)
+
+WEBKIT_VERSION = (
+    '601.4.4',
+    '601.5.17',
+    '601.6.17',
+    '601.7.1',
+    '601.7.8',
+    '602.1.50',
+    '602.2.14',
+    '602.3.12',
+    '602.4.8',
+    '603.1.30',
+    '603.2.4',
+    '603.3.8',
+)
+
+SAFARI_VERSION = (
+    '10.1.2',
+    '11.1.2',
+    '12.0.2',
+)
+
+IE_VERSION = (
+    # (numeric ver, string ver, trident ver) # release year
+    (8, 'MSIE 8.0', '4.0'),  # 2009
+    (9, 'MSIE 9.0', '5.0'),  # 2011
+    (10, 'MSIE 10.0', '6.0'),  # 2012
+    (11, 'MSIE 11.0', '7.0'),  # 2013
+)
+
+# https://en.wikipedia.org/wiki/Microsoft_Edge#Release_history
+EDGE_VERSION = (
+    '15.14986',
+    '15.15063',
+    '16.16299',
+    '17.17134',
+    '18.17763',
+)
+
+USER_AGENT_TEMPLATE = {
+    'firefox': (
+        'Mozilla/5.0'
+        ' ({system[ua_platform]}; rv:{app[build_version]})'
+        ' Gecko/{app[geckotrail]}'
+        ' Firefox/{app[build_version]}'
+    ),
+    'chrome': (
+        'Mozilla/5.0'
+        ' ({system[ua_platform]}) AppleWebKit/537.36'
+        ' (KHTML, like Gecko)'
+        ' Chrome/{app[build_version]} Safari/537.36'
+    ),
+    'chrome_android': (
+        'Mozilla/5.0'
+        ' ({system[ua_platform]}) AppleWebKit/537.36'
+        ' (KHTML, like Gecko)'
+        ' Chrome/{app[build_version]} Mobile Safari/537.36'
+    ),
+    'safari_ios': (
+        'Mozilla/5.0'
+        ' (iPhone; CPU iPhone OS {system[ua_platform]} like Mac OS X) AppleWebKit/{app[webkit_version]}'
+        ' (KHTML, like Gecko)'
+        ' Version/{system[version]} Mobile/{system[platform_ver]} Safari/{app[safari_version]}'
+    ),
+    'safari_mac': (
+        'Mozilla/5.0'
+        ' ({system[ua_platform]}) AppleWebKit/{app[webkit_version]}'
+        ' (KHTML, like Gecko)'
+        ' Version/{app[build_version]} Safari/{app[webkit_version]}'
+    ),
+    # https://developer.chrome.com/multidevice/user-agent#chrome_for_ios_user_agent
+    'chrome_ios': (
+        'Mozilla/5.0'
+        ' (iPhone; CPU iPhone OS {system[ua_platform]} like Mac OS X) AppleWebKit/601.4.4'
+        ' (KHTML, like Gecko)'
+        ' CriOS/{app[build_version]} Mobile/{system[platform_ver]} Safari/601.4'
+    ),
+    # https://cloud.tencent.com/developer/section/1190015
+    'firefox_ios': (
+        'Mozilla/5.0'
+        ' (iPhone; CPU iPhone OS {system[ua_platform]} like Mac OS X) AppleWebKit/601.4.4'
+        ' (KHTML, like Gecko)'
+        ' FxiOS/{app[build_version]} Mobile/{system[platform_ver]} Safari/601.4'
+    ),
+    'ie_less_11': (
+        'Mozilla/5.0'
+        ' (compatible; {app[build_version]}; {system[ua_platform]};'
+        ' Trident/{app[trident_version]})'
+    ),
+    'ie_11': (
+        'Mozilla/5.0'
+        ' ({system[ua_platform]}; Trident/{app[trident_version]};'
+        ' rv:11.0) like Gecko'
+    ),
+    'edge': (
+        'Mozilla/5.0'
+        ' ({system[ua_platform]}) AppleWebKit/537.36'
+        ' (KHTML, like Gecko)'
+        ' Chrome/64.0.3282.140 Safari/537.36'
+        ' Edge/{app[build_version]}'
+    ),
+    # https://deviceatlas.com/blog/mobile-browser-user-agent-strings
+    'opera': (
+        'Mozilla/5.0'
+        ' ({system[ua_platform]}) AppleWebKit/537.36'
+        ' (KHTML, like Gecko)'
+        ' Chrome/64.0.3282.140 Safari/537.36'
+        ' OPR/{app[build_version]}'
+    ),
+    'opera_android': (
+        'Mozilla/5.0'
+        ' ({system[ua_platform]}) AppleWebKit/537.36'
+        ' (KHTML, like Gecko)'
+        ' Chrome/64.0.3282.140 Mobile Safari/537.36'
+        ' OPR/{app[build_version]}'
+    ),
+    'opera_ios': (
+        'Mozilla/5.0'
+        ' (iPhone; CPU iPhone OS {system[ua_platform]} like Mac OS X) AppleWebKit/601.4.4'
+        ' (KHTML, like Gecko)'
+        ' OPiOS/{app[build_version]} Mobile/{system[platform_ver]} Safari/601.4'
+    ),
+}
+
+PACKAGE_DIR = os.path.dirname(os.path.realpath(__file__))
+ANDROID_DEV = json.load(
+    open(os.path.join(PACKAGE_DIR, 'static', 'android_dev.json')))
+ANDROID_BUILD = json.load(
+    open(os.path.join(PACKAGE_DIR, 'static', 'android_build.json')))
+IOS_VERSION = json.load(
+    open(os.path.join(PACKAGE_DIR, 'static', 'ios.json')))
+
+# http://ftp.opera.com/pub/opera/desktop/
+OPERA_BUILD = json.load(
+    open(os.path.join(PACKAGE_DIR, 'static', 'opera_build.json')))
---
+++ pkgs/user_agent/user_agent.py
@@ -0,0 +1,251 @@
+"""
+This module is for generating random, valid web navigator's User-Agent HTTP headers.
+
+Functions:
+* generate_user_agent: generates User-Agent HTTP header
+
+"""
+
+
+from itertools import product
+from random import choice, randint
+
+import six
+from .exceptions import InvalidOption
+from .constants import *
+
+
+__all__ = ['generate_user_agent']
+
+
+def get_chrome_build():
+    build = choice(CHROME_BUILD)
+    return '%d.0.%d.%d' % (
+        build[0],
+        randint(build[1], build[2]),
+        randint(0, 99),
+    )
+
+
+def fix_mac_platform(platform, navigator_id):
+    """
+    Chrome, Opera and Safari on Mac OS adds minor version number and uses
+    underscores instead of dots. E.g. platform for Firefox will be: 'Intel Mac OS X 10.11'
+    but for Chrome, Opera and Safari it will be 'Intel Mac OS X 10_11_6'.
+    """
+    ver = platform.split('OS X ')[1]
+    build_range = range(*MACOSX_CHROME_BUILD_RANGE[ver])
+    build = choice(build_range)
+    mac_ver = ver.replace('.', '_') + '_' + str(build)
+    if navigator_id == 'safari':
+        return 'Macintosh; U; Intel Mac OS X %s; zh-cn' % mac_ver
+    else:
+        return 'Macintosh; Intel Mac OS X %s' % mac_ver
+
+
+def build_system_components(os_id, navigator_id):
+    """For given os_id build random platform."""
+
+    if os_id == 'win':
+        if navigator_id == 'edge':
+            platform = 'Windows NT 10.0'
+        else:
+            platform = choice(OS_PLATFORM['win'])
+        cpu = choice(OS_CPU['win'])
+        if cpu:
+            platform = '%s; %s' % (platform, cpu)
+        res = {'ua_platform': platform}
+    elif os_id == 'linux':
+        platform = '%s %s' % (
+            choice(OS_PLATFORM['linux']), choice(OS_CPU['linux']))
+        res = {'ua_platform': platform}
+    elif os_id == 'mac':
+        platform = choice(OS_PLATFORM['mac'])
+        if navigator_id in ('chrome', 'safari', 'opera'):
+            platform = fix_mac_platform(platform, navigator_id)
+        res = {'ua_platform': platform}
+    elif os_id == 'android':
+        platform = choice(OS_PLATFORM['android'])
+        if navigator_id == 'firefox':
+            ua_platform = '%s; Mobile' % platform
+        elif navigator_id in ('chrome', 'opera'):
+            dev_id = choice(ANDROID_DEV)
+            bulid_id = choice(ANDROID_BUILD)
+            device_id = '%s Build/%s' % (dev_id, bulid_id)
+            ua_platform = 'Linux; %s; %s' % (platform, device_id)
+        res = {'ua_platform': platform}
+    elif os_id == 'ios':
+        platform = choice(list(IOS_VERSION))
+        res = {
+            'ua_platform': platform.replace('.', '_'),
+            'version': platform.split('.')[0] + '.0',
+            'platform_ver': IOS_VERSION[platform],
+        }
+    return res
+
+
+def build_app_components(os_id, navigator_id):
+    """For given navigator_id build app features."""
+
+    if navigator_id == 'firefox':
+        build_version = choice(FIREFOX_VERSION)
+        if os_id in ('win', 'linux', 'mac'):
+            geckotrail = '20100101'
+        else:
+            geckotrail = build_version
+        res = {
+            'build_version': build_version,
+            'geckotrail': geckotrail,
+        }
+    elif navigator_id == 'chrome':
+        res = {
+            'build_version': get_chrome_build(),
+        }
+    elif navigator_id == 'ie':
+        num_ver, build_version, trident_version = choice(IE_VERSION)
+        res = {
+            'build_version': build_version,
+            'trident_version': trident_version,
+        }
+    elif navigator_id == 'edge':
+        res = {
+            'build_version': choice(EDGE_VERSION),
+        }
+    elif navigator_id == 'safari':
+        webkit_version = choice(WEBKIT_VERSION)
+        res = {
+            'webkit_version': webkit_version,
+            'safari_version': webkit_version[:5],
+            'build_version': choice(SAFARI_VERSION),
+        }
+    elif navigator_id == 'opera':
+        res = {
+            'build_version': choice(OPERA_BUILD),
+        }
+    return res
+
+
+def get_option_choices(opt_name, opt_value, default_value, all_choices):
+    """
+    Generate possible choices for the option `opt_name`
+    limited to `opt_value` value with default value
+    as `default_value`
+    """
+
+    choices = []
+    if isinstance(opt_value, six.string_types):
+        choices = [opt_value]
+    elif isinstance(opt_value, (list, tuple)):
+        choices = list(opt_value)
+    elif opt_value is None:
+        choices = default_value
+    else:
+        raise InvalidOption('Option %s has invalid'
+                            ' value: %s' % (opt_name, opt_value))
+    if 'all' in choices:
+        choices = all_choices
+    for item in choices:
+        if item not in all_choices:
+            raise InvalidOption('Choices of option %s contains invalid'
+                                ' item: %s' % (opt_name, item))
+    return choices
+
+
+def pick_config_ids(device_type, os, navigator):
+    """
+    Select one random pair (device_type, os_id, navigator_id) from
+    all possible combinations matching the given os and
+    navigator filters.
+
+    :param os: allowed os(es)
+    :type os: string or list/tuple or None
+    :param navigator: allowed browser engine(s)
+    :type navigator: string or list/tuple or None
+    :param device_type: limit possible oses by device type
+    :type device_type: list/tuple or None, possible values:
+        "desktop", "smartphone", "all"
+    """
+
+    if os is None:
+        default_dev_types = ['desktop']
+    else:
+        default_dev_types = list(DEVICE_TYPE_OS.keys())
+    dev_type_choices = get_option_choices(
+        'device_type', device_type, default_dev_types,
+        list(DEVICE_TYPE_OS.keys())
+    )
+    os_choices = get_option_choices('os', os, list(OS_NAVIGATOR.keys()),
+                                    list(OS_NAVIGATOR.keys()))
+    nav_choices = get_option_choices('navigator', navigator,
+                                     list(NAVIGATOR_OS.keys()),
+                                     list(NAVIGATOR_OS.keys()))
+
+    variants = []
+    for dev, os, nav in product(dev_type_choices, os_choices,
+                                nav_choices):
+
+        if (os in DEVICE_TYPE_OS[dev]
+                and nav in DEVICE_TYPE_NAVIGATOR[dev]
+                and nav in OS_NAVIGATOR[os]):
+            variants.append((dev, os, nav))
+    if not variants:
+        raise InvalidOption('Options device_type, os and navigator'
+                            ' conflicts with each other')
+    device_type, os_id, navigator_id = choice(variants)
+
+    assert os_id in OS_PLATFORM
+    assert navigator_id in NAVIGATOR_OS
+    assert device_type in DEVICE_TYPE_OS
+
+    return device_type, os_id, navigator_id
+
+
+def choose_ua_template(os_id, navigator_id, app):
+    tpl_name = navigator_id
+    if navigator_id == 'ie':
+        tpl_name = 'ie_11' if app['build_version'] == 'MSIE 11.0' else 'ie_less_11'
+    elif navigator_id == 'chrome':
+        if os_id == 'android':
+            tpl_name = 'chrome_android'
+        elif os_id == 'ios':
+            tpl_name = 'chrome_ios'
+    elif navigator_id == 'firefox' and os_id == 'ios':
+        tpl_name = 'firefox_ios'
+    elif navigator_id == 'safari':
+        tpl_name = 'safari_mac' if os_id == 'mac' else 'safari_ios'
+    elif navigator_id == 'opera':
+        if os_id == 'android':
+            tpl_name = 'opera_android'
+        elif os_id == 'ios':
+            tpl_name = 'opera_ios'
+    return USER_AGENT_TEMPLATE[tpl_name]
+
+
+def generate_user_agent(os=None, navigator=None, device_type=None):
+    """
+    Generates HTTP User-Agent header
+
+    :param os: limit list of os for generation, possible values:
+        "win", "linux", "mac", "android", "ios", "all"
+    :type os: string or list/tuple or None
+    :param navigator: limit list of browser engines for generation, possible values:
+        "chrome", "firefox", "ie", "edge", "safari", "opera", "all"
+    :type navigator: string or list/tuple or None
+    :param device_type: limit possible oses by device type
+    :type device_type: list/tuple or None, possible values:
+        "desktop", "smartphone", "all"
+
+    :return: User-Agent string
+    :rtype: string
+    :raises InvalidOption: if could not generate user-agent for
+        any combination of allowed oses and navigators
+    :raise InvalidOption: if any of passed options is invalid
+    """
+
+    device_type, os_id, navigator_id = pick_config_ids(
+        device_type, os, navigator)
+    system = build_system_components(os_id, navigator_id)
+    app = build_app_components(os_id, navigator_id)
+    ua_template = choose_ua_template(os_id, navigator_id, app)
+    user_agent = ua_template.format(system=system, app=app)
+    return user_agent
