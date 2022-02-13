af5f1bcfa24cf6c0c3ee13e06bf9f3eacac50de1
Rustle Karl <fu.jiawei@outlook.com>
2022-02-13 22:33:22 +0800

    add: logger template

--- project_scaffold/templates/python/{{PYTHON_MODULE}}.logger.py
+++ project_scaffold/templates/python/{{PYTHON_MODULE}}.logger.py
@@ -1,1 +1,32 @@
 {{PYTHON_HEADER}}
+
+import logging
+from logging.handlers import RotatingFileHandler
+
+from .version import __project__
+
+DEBUG = True
+
+log = logging.getLogger(__project__)
+
+# final level = max(Logger's level, Handler's Level)
+log.setLevel(logging.DEBUG)
+
+if DEBUG:
+    console = logging.StreamHandler()
+    console.setLevel(logging.DEBUG)
+    console.setFormatter(
+        logging.Formatter(
+            "%(asctime)s [%(levelname)s] %(filename)s:%(lineno)s %(message)s"
+        )
+    )
+    log.addHandler(console)
+else:
+    file = logging.handlers.RotatingFileHandler(
+        mode="w",
+        encoding="utf-8",
+        maxBytes=(1 << 20) * 50,  # MB
+        backupCount=30,
+    )
+    file.setLevel(logging.INFO)
+    log.addHandler(file)
