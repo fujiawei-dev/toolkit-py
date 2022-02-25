243f5e921e37da416321d3741feaada30e6dc580
Rustle Karl <fu.jiawei@outlook.com>
2022-02-25 21:25:25 +0800

    update: validator

--- project_scaffold/templates/golang/internal/form/example.go
+++ project_scaffold/templates/golang/internal/form/example.go
@@ -2,48 +2,56 @@
 
 package {{GOLANG_PACKAGE}}
 
+// ExampleCreate 表单验证示例
+// https://github.com/go-playground/validator
 type ExampleCreate struct {
-	// 嵌套结构验证
-	Array []string          `binding:"required,gt=0,dive,max=20" json:"array,omitempty"`
-	Map   map[string]string `binding:"required,gt=0,dive,keys,max=10,endkeys,required,max=2" json:"map,omitempty"`
-
-	IndexField       int `binding:"omitempty,min=5,max=128" example:"100" json:"index_field,omitempty"`
-	UniqueField      int `json:"unique_field,omitempty"`
-	UniqueIndexField int `json:"unique_index_field,omitempty"`
-
 	// 限制字符串长度
-	ShortStringField string `binding:"required,min=2,max=16" example:"短字符串字段" json:"short_string_field,omitempty"`
-	LongStringField  string `binding:"required,min=64,max=256" example:"长字符串字段" json:"long_string_field,omitempty"`
-	LongTextField    string `binding:"required,min=512" example:"超长文本字段" json:"long_text_field,omitempty"`
+	ShortStringField string `binding:"required,max=16" example:"短字符串字段" json:"short_string_field"`
+	LongStringField  string `binding:"required,min=16,max=512" example:"长字符串字段" json:"long_string_field"`
+	LongTextField    string `binding:"required,min=512" example:"超长文本字段" json:"long_text_field"`
+	CommentField     string `binding:"required,len=5" example:"定长字符串" json:"comment_field"`
+
+	// 数值比较
+	// 大于 & 小于
+	IndexField int `binding:"gt=5,lt=128" example:"100" json:"index_field"`
+	// 大于等于 & 小于等于
+	UniqueField int `binding:"gte=5,lte=100" json:"unique_field"`
+	// 不等于
+	UniqueIndexField int `binding:"ne=1" json:"unique_index_field"`
+
+	// 嵌套结构验证
+	// 表示列表不为空，且元素的长度不超过 20
+	StringList []string `binding:"required,gt=0,dive,max=20" json:"string_list"`
+	// 表示字典不为空，且键的长度不超过 10，值的长度不超过 20
+	StringDict map[string]string `binding:"required,gt=0,dive,keys,max=10,endkeys,required,max=20" json:"string_dict"`
 
 	// 自定义枚举值
-	IntegerField int `binding:"oneof=20 50 100" example:"50" json:"integer_field,omitempty"`
+	IntegerField int `binding:"oneof=20 50 100" example:"50" json:"integer_field"`
 
-	UnsignedIntegerField uint    `json:"unsigned_integer_field,omitempty"`
-	Float64Field         float64 `json:"float_64_field,omitempty"`
-	Float32Field         float32 `json:"float_32_field,omitempty"`
-	BinaryField          []byte  `json:"binary_field,omitempty"`
+	// 字段的默认值
+	DefaultField string `example:"默认字段" json:"default_field,default=ignore"`
 
-	DefaultField string `example:"默认字段" json:"default_field,omitempty"`
+	// 字段间关系
+	UnsignedIntegerField uint    `json:"unsigned_integer_field"`
+	Float64Field         float64 `json:"float_64_field"`
+	// 在 UnsignedIntegerField 或者 Float32Field 存在时，必须存在
+	Float32Field float32 `binding:"required_with=UnsignedIntegerField Float32Field" json:"float_32_field"`
+	// 在 UnsignedIntegerField 或者 Float32Field 不存在时，必须存在
+	BinaryField []byte `binding:"required_without_all=UnsignedIntegerField Float32Field" json:"binary_field"`
+	// 必须不等于 UniqueField 的值
+	CheckField int `binding:"required,nefield=UniqueField" json:"check_field"`
 
-	CheckField   int    `json:"check_field,omitempty"`
-	CommentField string `binding:"required,len=5" example:"字符串定长" json:"comment_field,omitempty"`
-
-	AllowReadAndCreate   string `binding:"omitempty,uuid4" json:"allow_read_and_create,omitempty"`
-	AllowReadAndUpdate   string `json:"allow_read_and_update,omitempty"`
-	AllowCreateAndUpdate string `json:"allow_create_and_update,omitempty"`
-	ReadOnly             string `json:"read_only,omitempty"`
-	CreateOnly           string `json:"create_only,omitempty"`
-	IgnoreWriteAndRead   string `json:"ignore_write_and_read,omitempty"`
-	IgnoreMigration      string `json:"ignore_migration,omitempty"`
+	ExampleUpdate
 }
 
 type ExampleUpdate struct {
-	AllowReadAndCreate   string `binding:"omitempty,uuid4" json:"allow_read_and_create,omitempty"`
-	AllowReadAndUpdate   string `json:"allow_read_and_update,omitempty"`
-	AllowCreateAndUpdate string `json:"allow_create_and_update,omitempty"`
-	ReadOnly             string `json:"read_only,omitempty"`
-	CreateOnly           string `json:"create_only,omitempty"`
-	IgnoreWriteAndRead   string `json:"ignore_write_and_read,omitempty"`
-	IgnoreMigration      string `json:"ignore_migration,omitempty"`
+	// 必填项/条件可填
+	// omitempty 表示变量可以不填，但是填的时候必须满足条件
+	AllowReadAndCreate   string `binding:"required,ip4_addr" json:"allow_read_and_create"`
+	AllowReadAndUpdate   string `binding:"omitempty,url" json:"allow_read_and_update"`
+	AllowCreateAndUpdate string `binding:"omitempty,email" json:"allow_create_and_update"`
+	ReadOnly             string `binding:"required,alphanum" json:"read_only"`
+	CreateOnly           string `binding:"required,uuid4" json:"create_only"`
+	IgnoreWriteAndRead   string `binding:"omitempty,startswith=go" json:"ignore_write_and_read"`
+	IgnoreMigration      string `binding:"omitempty,lowercase" json:"ignore_migration"`
 }
--- project_scaffold/templates/golang/internal/form/pager.go
+++ project_scaffold/templates/golang/internal/form/pager.go
@@ -6,9 +6,10 @@ package {{GOLANG_PACKAGE}}
 
 // Pager 分页处理
 type Pager struct {
-	Page      int   `json:"page" form:"page,default=1" url:"page" validate:"gte=1"`                          // 页码
-	PageSize  int   `json:"page_size" form:"page_size,default=50" url:"page_size" validate:"gte=10,lte=100"` // 每页数量
-	TotalRows int64 `json:"total_rows"`                                                                      // 总行数
+	Page      int   `json:"page" form:"page" url:"page" binding:"gte=1"`                         // 页码
+	PageSize  int   `json:"page_size" form:"page_size" url:"page_size" binding:"gte=10,lte=100"` // 每页数量
+	Order     int   `json:"order" form:"order" binding:"oneof=0 1 2 3"`                          // 排序顺序
+	TotalRows int64 `json:"total_rows"`                                                          // 总行数
 }
 
 func (p Pager) Offset() int {
