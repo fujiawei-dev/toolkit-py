8bc5dcc7ad046af30e9d464492fb4aa29db16bc8
Rustle Karl <fu.jiawei@outlook.com>
2022-03-09 22:08:48 +0800

    update: a better way to exit for qt5 console template

--- project_scaffold/templates/qt5/main.console.cpp
+++ project_scaffold/templates/qt5/main.console.cpp
@@ -2,9 +2,40 @@
 
 #include <QCoreApplication>
 #include <QDebug>
+#include <QTimer>
+
+class Task : public QObject {
+    Q_OBJECT
+public:
+    Task(QObject *parent = nullptr) : QObject(parent) {}
+
+public slots:
+    void run() {
+        qInfo() << "Running...";
+
+        emit finished();
+
+        qInfo() << "I thought I'd finished!";
+    }
+
+signals:
+    void finished();
+};
+
+#include "main.moc"
 
 int main(int argc, char *argv[]) {
-  QCoreApplication a(argc, argv);
-  qDebug() << "Hello World";
-  return QCoreApplication::exec();
+    // https://forum.qt.io/topic/55226/how-to-exit-a-qt-console-app-from-an-inner-class-solved
+    QCoreApplication a(argc, argv);
+
+    // Task  parented to the application so that it will be deleted by the application.
+    Task *task = new Task(&a);
+
+    // This will cause the application to exit when the task signals finished.
+    QObject::connect(task, SIGNAL(finished()), &a, SLOT(quit()));
+
+    // This will run the task from the application event loop.
+    QTimer::singleShot(0, task, SLOT(run()));
+
+    return QCoreApplication::exec();
 }
