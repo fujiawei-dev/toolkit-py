4a048baeca552c8ade2c994487bb6e04b602140e
Rustle Karl <fu.jiawei@outlook.com>
2022-03-15 23:39:38 +0800

    update: print all platforms

--- create_config_file/alias.py
+++ create_config_file/alias.py
@@ -7,7 +7,7 @@ LastEditTime: 2022.02.13 14:41
 import yaml
 from jinja2 import Template
 
-from .common import SEPARATE, is_windows, join_user, writer
+from .common import SEPARATE, join_user, writer
 from .templates import COMMON_PATH
 
 _powershell_template = """\
@@ -28,41 +28,49 @@ alias {{key}}='{{value}}'
 def alias(read_only=True):
     aliases = yaml.safe_load((COMMON_PATH / "aliases.yaml").open(encoding="utf-8"))
 
-    if is_windows():
-        confs = [join_user("Documents/PowerShell/Microsoft.PowerShell_profile.ps1")]
-        content = Template(_powershell_template).render(
-            aliases={
-                **aliases.get("common", {}),
-                **aliases.get("windows", {}),
-            }
-        )
-        read_only = True
-        message = SEPARATE + f"code $profile\n\n{content}\n\n& $profile\n" + SEPARATE
-    else:
-        confs = [
-            join_user(".config/fish/config.fish"),
-            join_user(".profile"),
-            join_user(".zshrc"),
-        ]
-        content = Template(_bash_templates).render(
-            aliases={
-                **aliases.get("common", {}),
-                **aliases.get("linux", {}),
-            }
-        )
-        message = (
-            SEPARATE
-            + content.strip()
-            + "\n\n"
-            + "vim ~/.config/fish/config.fish\n"
-            + "vim ~/.profile\n"
-            + "vim ~/.zshrc\n"
-            + "\n"
-            + "source ~/.config/fish/config.fish\n"
-            + "source ~/.profile\n"
-            + "source ~/.zshrc\n"
-            + SEPARATE
-        )
+    # For Window
+    confs = [join_user("Documents/PowerShell/Microsoft.PowerShell_profile.ps1")]
+    content = Template(_powershell_template).render(
+        aliases={
+            **aliases.get("common", {}),
+            **aliases.get("windows", {}),
+        }
+    )
+    message = SEPARATE + f"code $profile\n\n{content}\n\n& $profile\n" + SEPARATE
+
+    writer(
+        *confs,
+        content=content,
+        read_only=True,
+        message=message,
+        append=True,
+    )
+
+    # For Linux
+    confs = [
+        join_user(".config/fish/config.fish"),
+        join_user(".profile"),
+        join_user(".zshrc"),
+    ]
+    content = Template(_bash_templates).render(
+        aliases={
+            **aliases.get("common", {}),
+            **aliases.get("linux", {}),
+        }
+    )
+    message = (
+        SEPARATE
+        + content.strip()
+        + "\n\n"
+        + "vim ~/.config/fish/config.fish\n"
+        + "vim ~/.profile\n"
+        + "vim ~/.zshrc\n"
+        + "\n"
+        + "source ~/.config/fish/config.fish\n"
+        + "source ~/.profile\n"
+        + "source ~/.zshrc\n"
+        + SEPARATE
+    )
 
     writer(
         *confs,
