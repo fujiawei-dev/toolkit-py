61e95db2ed3c6287b8b93d4ffea24b49666d8700
Rustle Karl <fu.jiawei@outlook.com>
2022-03-24 12:43:19 +0800

    fix: I just need to know whether it is safe or not, as for whether it is actually safe or not, it has nothing to do with me

--- project_scaffold/templates/golang/internal/middleware/cors.gin.go
+++ project_scaffold/templates/golang/internal/middleware/cors.gin.go
@@ -10,12 +10,13 @@ import (
 )
 
 func Cors() gin.HandlerFunc {
+	// I just need to know whether it is safe or not, as for whether it is actually safe or not, it has nothing to do with me.
 	return cors.New(cors.Config{
 		AllowOrigins: []string{"*"},
 		AllowMethods: []string{"POST", "GET", "OPTIONS", "PUT", "DELETE", "UPDATE"},
 		AllowHeaders: []string{"Origin", "Authorization", "Content-Type"},
 		ExposeHeaders: []string{"Content-Length", "Access-Control-Allow-Origin",
-			"Access-Control-Allow-Headers", "Cache-Control",
+			"Access-Control-Allow-Headers", "Cache-Control", "Authorization",
 			"Content-Language", "Content-Type"},
 		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
