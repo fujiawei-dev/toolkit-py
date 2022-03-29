669a2073f8ec6f83099b42af47a6bff4478f06e9
Rustle Karl <fu.jiawei@outlook.com>
2022-03-29 10:40:15 +0800

    add: generate swagger docs at the same time

--- project_scaffold/templates/golang/internal/api/entity_template.gin.go
+++ project_scaffold/templates/golang/internal/api/entity_template.gin.go
@@ -23,6 +23,16 @@ func init() {
 	AddRouteRegistrar(GetEntityTemplates)
 }
 
+// PostEntityTemplate
+// @Summary      创建
+// @Description  创建
+// @Tags         实体管理
+// @Accept       json
+// @Security     ApiKeyAuth
+// @Param        object  body  form.EntityTemplateCreate  false  "参数"
+// @Produce      json
+// @Success      200  {object}  query.Response  "操作成功"
+// @Router       /entity_template [post]
 func PostEntityTemplate(router *gin.RouterGroup) {
 	router.POST("/entity_template", conf.JWTMiddleware(), func(c *gin.Context) {
 		if _, pass := Auth(c, acl.ResourceEntityTemplates, acl.ActionCreate); !pass {
@@ -52,6 +62,17 @@ func PostEntityTemplate(router *gin.RouterGroup) {
 	})
 }
 
+// PutEntityTemplate
+// @Summary      修改
+// @Description  修改
+// @Tags         实体管理
+// @Accept       json
+// @Security     ApiKeyAuth
+// @Param        id      path  int              true   "ID"
+// @Param        object  body  form.EntityTemplateUpdate  false  "参数"
+// @Produce      json
+// @Success      200  {object}  query.Response  "操作成功"
+// @Router       /entity_template/{id} [put]
 func PutEntityTemplate(router *gin.RouterGroup) {
 	router.PUT("/entity_template/:id", conf.JWTMiddleware(), func(c *gin.Context) {
 		if _, pass := Auth(c, acl.ResourceEntityTemplates, acl.ActionUpdate); !pass {
@@ -97,6 +118,16 @@ func PutEntityTemplate(router *gin.RouterGroup) {
 	})
 }
 
+// DeleteEntityTemplate
+// @Summary      删除
+// @Description  删除
+// @Tags         实体管理
+// @Accept       json
+// @Security     ApiKeyAuth
+// @Param        id  path  int  true  "ID"
+// @Produce      json
+// @Success      200  {object}  query.Response  "操作成功"
+// @Router       /unit/{id} [delete]
 func DeleteEntityTemplate(router *gin.RouterGroup) {
 	router.DELETE("/entity_template/:id", conf.JWTMiddleware(), func(c *gin.Context) {
 		if _, pass := Auth(c, acl.ResourceEntityTemplates, acl.ActionDelete); !pass {
@@ -125,6 +156,15 @@ func DeleteEntityTemplate(router *gin.RouterGroup) {
 	})
 }
 
+// GetEntityTemplate
+// @Summary      获取
+// @Description  获取
+// @Tags         实体管理
+// @Accept       json
+// @Param        id  path  int  true  "ID"
+// @Produce      json
+// @Success      200  {object}  query.Response{result=query.EntityTemplateResult}  "操作成功"
+// @Router       /entity_template/{id} [get]
 func GetEntityTemplate(router *gin.RouterGroup) {
 	router.GET("/entity_template/:id", conf.JWTMiddleware(), func(c *gin.Context) {
 		if _, pass := Auth(c, acl.ResourceEntityTemplates, acl.ActionRead); !pass {
@@ -148,6 +188,18 @@ func GetEntityTemplate(router *gin.RouterGroup) {
 	})
 }
 
+// GetEntityTemplate
+// @Summary      实体列表
+// @Description  获取实体列表
+// @Tags         实体管理
+// @Accept       json
+// @Param        page        query  int     false  "页码"    default(1)
+// @Param        page_size   query  int     false  "每页数量"  Enums(10, 20)  default(10)
+// @Param        time_begin  query  string  false  "开始时间，比如 2021-10-01"
+// @Param        time_end    query  string  false  "结束时间，比如 2022-10-01"
+// @Produce      json
+// @Success      200  {object}  query.Response{result=Result{pager=form.Pager,list=[]query.EntityTemplateResult}}  "操作成功"
+// @Router       /entity_templates [get]
 func GetEntityTemplates(router *gin.RouterGroup) {
 	router.GET("/entity_templates", conf.JWTMiddleware(), func(c *gin.Context) {
 		if _, pass := Auth(c, acl.ResourceEntityTemplates, acl.ActionSearch); !pass {
