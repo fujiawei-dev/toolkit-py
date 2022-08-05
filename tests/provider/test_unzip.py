3fe2ae67a8acabd3e867f0f14a63d761badaa48f
Rustle Karl <fu.jiawei@outlook.com>
2022-08-05 13:59:19 +0800

    fix: change to temporary directory

--- tests/provider/test_unzip.py
+++ tests/provider/test_unzip.py
@@ -1,3 +1,6 @@
+import os
+import tempfile
+
 from tests.provider.test_unzip_data import (
     assert_compressed_files,
     create_compressed_files,
@@ -6,22 +9,23 @@ from toolkit.provider.unzip import Unzipper, delete_empty_directories
 
 
 def test_create_compressed_files():
-    create_compressed_files("data")
+    create_compressed_files()
 
 
 def test_unzipper():
-    unzipper = Unzipper()
+    with tempfile.TemporaryDirectory() as temp_dir:
+        unzipper = Unzipper()
 
-    path = "data"
-    move_to = "data/unzipped"
+        path = os.path.join(temp_dir, "path")
+        move_to = os.path.join(temp_dir, "move_to")
 
-    path = create_compressed_files(
-        path=path,
-        passwords=unzipper.passwords[1:10],
-    )
+        path = create_compressed_files(
+            path=path,
+            passwords=unzipper.passwords[1:10],
+        )
 
-    unzipper.run(path, move_to)
+        unzipper.run(path, move_to)
 
-    assert_compressed_files(move_to, path)
+        assert_compressed_files(move_to, path)
 
-    delete_empty_directories(path)
+        delete_empty_directories(path)
