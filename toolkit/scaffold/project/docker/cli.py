fb1c8c19e3089221017bf41c6771ae0a606978db
Rustle Karl <fu.jiawei@outlook.com>
2022-08-07 17:44:17 +0800

    update: simplified code

--- toolkit/scaffold/project/docker/cli.py
+++ toolkit/scaffold/project/docker/cli.py
@@ -2,8 +2,7 @@ import sys
 
 import click
 
-from toolkit.render.cutter import generate_rendered_files_recursively
-from toolkit.scaffold.project import context
+from toolkit.scaffold.project.command import generate_create_project_command
 from toolkit.scaffold.project.docker.compose import create_compose
 from toolkit.scaffold.project.template import TEMPLATE_DOCKER_PATH
 
@@ -13,30 +12,12 @@ def create_docker_project():
     pass
 
 
-@create_docker_project.command(
-    name="cli",
-    help="Create a docker-cli project scaffold.",
+create_cli = generate_create_project_command(
+    command_help="Create a docker-cli project scaffold.",
+    template_path=TEMPLATE_DOCKER_PATH / "cli",
 )
-@click.option(
-    "--project-path",
-    type=click.Path(exists=True, file_okay=False),
-    default=".",
-    help="Project path.",
-)
-@click.option("--overwrite", is_flag=True, help="Overwrite existing files.")
-def create_cli(project_path: str, overwrite: bool):
-    user_input_context = context.get_user_input_context()
-    ignored_items = context.get_ignored_items({}, [])
-    template_path = TEMPLATE_DOCKER_PATH / "cli"
 
-    generate_rendered_files_recursively(
-        template_path=template_path,
-        project_path=project_path,
-        user_input_context=user_input_context,
-        ignored_items=ignored_items,
-        skip_if_file_exists=not overwrite,
-    )
-
+create_docker_project.add_command(create_cli, "cli")
 
 create_docker_project.add_command(create_compose, "compose")
 
--- toolkit/scaffold/project/docker/compose.py
+++ toolkit/scaffold/project/docker/compose.py
@@ -1,31 +1,7 @@
-import click
-
-from toolkit.render.cutter import generate_rendered_files_recursively
-from toolkit.scaffold.project import context
+from toolkit.scaffold.project.command import generate_create_project_command
 from toolkit.scaffold.project.template import TEMPLATE_DOCKER_PATH
 
-
-@click.command(
-    name="compose",
-    help="Create a docker-compose project scaffold.",
-    context_settings={"ignore_unknown_options": True},
+create_compose = generate_create_project_command(
+    command_help="Create a docker-compose project scaffold.",
+    template_path=TEMPLATE_DOCKER_PATH / "compose",
 )
-@click.option(
-    "--project-path",
-    type=click.Path(exists=True, file_okay=False),
-    default=".",
-    help="Project path.",
-)
-@click.option("--overwrite", is_flag=True, help="Overwrite existing files.")
-def create_compose(project_path: str, overwrite: bool):
-    user_input_context = context.get_user_input_context()
-    ignored_items = context.get_ignored_items({}, [])
-    template_path = TEMPLATE_DOCKER_PATH / "compose"
-
-    generate_rendered_files_recursively(
-        template_path=template_path,
-        project_path=project_path,
-        user_input_context=user_input_context,
-        ignored_items=ignored_items,
-        skip_if_file_exists=not overwrite,
-    )
