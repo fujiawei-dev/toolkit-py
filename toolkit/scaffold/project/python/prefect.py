eb4c1e1e28b560ec5e47d599dd7171cb5adc8a0a
Rustle Karl <fu.jiawei@outlook.com>
2022-08-09 22:09:33 +0800

    fix: isort

--- toolkit/scaffold/project/python/prefect.py
+++ toolkit/scaffold/project/python/prefect.py
@@ -1,12 +1,10 @@
+from toolkit.config.context import USER_INPUT_CONTEXT
+from toolkit.scaffold.project.command import generate_create_project_command
 from toolkit.scaffold.project.python.context import (
+    PYTHON_CONTEXT,
     PYTHON_USER_INPUT_CONTEXT,
     python_user_input_context_hook,
-    PYTHON_CONTEXT,
 )
-
-from toolkit.config.context import USER_INPUT_CONTEXT
-
-from toolkit.scaffold.project.command import generate_create_project_command
 from toolkit.scaffold.project.template import TEMPLATE_PYTHON_PATH
 
 create_prefect = generate_create_project_command(
