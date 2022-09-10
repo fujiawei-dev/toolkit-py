9b91022e2f32f7f740573036816118a2ec29496a
Rustle Karl <fu.jiawei@outlook.com>
2022-09-10 12:49:36 +0800

    update: allow files to be overwritten

--- toolkit/scaffold/repeat/cli.py
+++ toolkit/scaffold/repeat/cli.py
@@ -21,6 +21,10 @@ import click
     and the content of each file is the same, you can use the following command:
 
     repeat -c "Hello World" %d.txt
+
+    More examples:
+
+    repeat -f --start 2 --stop 50 -c '##' 'q%03d.md'
     """
 )
 @click.option(
@@ -32,7 +36,7 @@ import click
 )
 @click.option("--mkdir", "-m", is_flag=True, help="Directory mode(create directories).")
 @click.option("--start", nargs=1, type=int, default=1, help="The starting number.")
-@click.option("--end", nargs=1, type=int, default=10, help="The ending number.")
+@click.option("--stop", nargs=1, type=int, default=10, help="The ending number.")
 @click.option("--step", nargs=1, type=int, default=1, help="The step size.")
 @click.option(
     "--content",
@@ -42,6 +46,7 @@ import click
     default="",
     help="The content to write to the file.",
 )
+@click.option("--force", "-f", is_flag=True, help="Force overwrite existing files.")
 @click.argument(
     "pattern",
     nargs=1,
@@ -52,21 +57,23 @@ def main(
     destination: str,
     mkdir: bool,
     start: int,
-    end: int,
+    stop: int,
     step: int,
     content: str,
+    force: bool,
     pattern: str,
 ):
-    for i in range(start, end, step):
+    for i in range(start, stop + 1, step):
         item = Path(destination) / (pattern % i)
-        if item.exists():
-            continue
-
         if mkdir:
-            item.mkdir(parents=True, exist_ok=True)
+            if not item.exists():
+                item.mkdir(parents=True, exist_ok=True)
+                click.echo(f"Created directory: {item}")
         else:
-            with open(item, "w", encoding="utf-8", newline="\n") as f:
-                f.write(content.strip() + "\n")
+            if not item.exists() or force:
+                with open(item, "w", encoding="utf-8", newline="\n") as f:
+                    f.write(content.strip() + "\n")
+                click.echo(f"Created file: {item}")
 
 
 if __name__ == "__main__":
